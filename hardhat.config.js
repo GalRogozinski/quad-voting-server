9f0e945d7fe6e025438b481c97643366c0c02b82
Gal Rogozinski <gal.rogozinski@iota.org>
2022-06-29 13:18:44 +0300

    try to fix hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,16 +9,14 @@ const config = {
     networks: {
         local: {
             url: "http://localhost:8545",
-            accounts: ["0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"
-            ],
+            accounts: ["0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"],
             loggingEnabled: false,
         },
         remote: {
             url: "http://quadvoting.twilightparadox.com:8545",
-            accounts: ["0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"
-            ],
+            accounts: ["0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"],
             loggingEnabled: false,
-        },
+        }
     },
     paths: {
         sources: "quad-voting-maci/contracts/contracts/",
