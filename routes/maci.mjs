cf840a391deb146398ee595464538e8354be772e
Gal Rogozinski <gal.rogozinski@iota.org>
2022-06-30 19:42:54 +0300

    add publish message route

--- routes/maci.mjs
+++ routes/maci.mjs
@@ -2,7 +2,9 @@ import {deployPollApi} from "../quad-voting-maci/cli/build/deployPollApi.js";
 import {signUpApi} from "../quad-voting-maci/cli/build/signUpApi.js";
 
 import express from "express";
-import {MACI_ADDRESS} from "../consts.mjs";
+import {MACI_ADDRESS, COO_PRIVATE_KEY} from "../consts.mjs";
+
+import {publishMessageApi} from "../quad-voting-maci/cli/ts/publishMessageApi";
 
 export let maciRouter = express.Router();
 
@@ -24,6 +26,15 @@ maciRouter.post('/signup', async (req, res,next) => {
     try {
         let stateID = await signUpApi(MACI_ADDRESS, req.body);
         res.json({stateID: stateID});
+    } catch (e) {
+        next(e);
+    }
+})
+
+maciRouter.post('/publishMessage', async (req, res, next) => {
+    try {
+        let txHash = await publishMessageApi(COO_PRIVATE_KEY, req.body);
+        res.json({receipt: txHash});
     } catch (e) {
         next(e);
     }
